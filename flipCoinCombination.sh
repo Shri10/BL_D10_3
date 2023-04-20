0fc128f1af7ad3488907ae73b6fb61c89da0c076
Shri10 <pethe.shriraj@gmail.com>
2023-04-20 17:41:42 +0530

    UC1

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,15 @@
-# UC1: Flipping a coin to display Heads or Tails
-flip_coin() {
-  if [ $((RANDOM % 2)) -eq 0 ]; then
-    echo "H"
-  else
-    echo "T"
-  fi
-}
+# UC2: Loop through multiple times and store the Singlet Combination in a dictionary
+declare -A singlet_count=( ["H"]=0 ["T"]=0 )
+num_flips=100
+
+for (( i=0; i<num_flips; i++ )); do
+  flip=$(flip_coin)
+  singlet_count[$flip]=$((singlet_count[$flip] + 1))
+done
+
+# Calculate and display the percentage of the Singlet Combination
+echo "UC2: Singlet Percentage"
+for key in "${!singlet_count[@]}"; do
+  percentage=$(echo "scale=2; (${singlet_count[$key]} / $num_flips) * 100" | bc)
+  echo "$key: $percentage%"
+done
